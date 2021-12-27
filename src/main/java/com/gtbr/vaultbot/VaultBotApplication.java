f62b80f7dd4c6a12f6c995666238b58bb583bded
jtomaz <jtomaz@minsait.com>
2021-12-27 18:29:14 -0300

    Initial Commit

---
+++ src/main/java/com/gtbr/vaultbot/VaultBotApplication.java
@@ -0,0 +1,10 @@
+package com.gtbr.vaultbot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultBotApplication {
+
+    public static void main(String[] args) {}
+
+}
